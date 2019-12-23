d401d0d9af0b275911d92a569be3b16533d6b764
Daniel W. Amerson <dwamerson@gmail.com>
2019-12-22 20:30:35 -0500

    Node script to create boilerplate code for Typescript-based React components.

---
+++ create-component.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const path = require('path');
+
+const createRoute = (componentsToCheck => {
+    let currentRouteToCheck = process.cwd();
+    componentsToCheck.forEach(componentToCheck => {
+        currentRouteToCheck = `${currentRouteToCheck}${path.sep}${componentToCheck}`;
+        if (!fs.existsSync(currentRouteToCheck)) {
+            console.log(`Creating directory ${currentRouteToCheck}...`);
+            fs.mkdirSync(currentRouteToCheck);
+        }
+    });
+});
+
+const createIndexFile = ((route, componentName) => {
+    const contents = `
+        import ${componentName}, { ${componentName}Props } from './${componentName}';
+
+        export default ${componentName};
+        export type ${componentName}Props = ${componentName}Props;
+    `;
+
+    fs.writeFileSync(`${route}${path.sep}index.ts`, contents, err => {
+        if (err) throw err;
+
+        console.log('index.ts persisted.');
+    });
+});
+
+const createStyleFile = ((route, componentName) => {
+    fs.writeFileSync(`${route}${path.sep}${componentName}.module.css`, '', err => {
+        if (err) throw err;
+
+        console.log(`${componentName}.module.css persisted.`);
+    });
+});
+
+const createComponentFile = ((route, componentName) => {
+    const contents = `
+    import React from 'react';
+
+    import styles from './${componentName}.module.css';
+
+    export type ${componentName}Props = {}
+
+    const ${componentName}: React.FunctionComponent<${componentName}Props> = (props) => {
+        return <></>;
+    }
+
+    export default ${componentName};
+    `;
+
+    fs.writeFileSync(`${route}${path.sep}${componentName}.tsx`, contents, err => {
+        if (err) throw err;
+
+        console.log(`${componentName}.tsx persisted.`);
+    });
+});
+
+const args = process.argv.slice(2);
+if (args.length != 1) {
+    console.log('Usage: node create-template.js <relative-path-to-component>');
+} else {
+    const componentRoute = args[0];
+    const pathComponents = componentRoute.split(path.sep);
+
+    let componentName = pathComponents[pathComponents.length - 1];
+    componentName = componentName.charAt(0).toLocaleUpperCase() + componentName.slice(1);
+
+    const componentDestination = `${process.cwd()}${path.sep}${componentRoute}`;
+
+    console.log(`Creating ${componentName} in ${componentDestination}...`);
+
+    createRoute(pathComponents);
+    createIndexFile(componentDestination, componentName);
+    createStyleFile(componentDestination, componentName);
+    createComponentFile(componentDestination, componentName);
+
+    console.log('Done!');
+}
